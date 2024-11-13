c6a842ab3e569d4fbb68dde9c4d79d64a2d91252
codewitch-honey-crisis <gaztoast@gmail.com>
2024-11-13 12:52:00 -0800

    improved example

--- Example/Program.cs
+++ Example/Program.cs
@@ -14,31 +14,22 @@ namespace Example
 	{
 		[CmdArg(Ordinal = 0)]
 		static TextReader[] inputs = { Console.In };
-		[CmdArg(Optional = true, Description = "The foo argument")]
-		static string[] foo = new string[] { "bar" };
-		[CmdArg(Description = "The test argument")]
-		static bool test;
+		[CmdArg(Optional = true, Description = "The width to wrap to in characters")]
+		static int wrap = Console.WindowWidth;
 		static void Main()
 		{
 			var switches = CliUtility.GetSwitches(typeof(Program));
-			CliUtility.PrintUsage(switches, 80);
+			CliUtility.PrintUsage(switches, wrap);
 			using (var result = CliUtility.ParseArguments(switches))
 			{
-				CliUtility.SetValues(switches, result, typeof(Program));
-				if (System.Diagnostics.Debugger.IsAttached)
-				{
-					// check the fields on the class now
-					System.Diagnostics.Debugger.Break();
-				}
+				CliUtility.SetValues(switches, result,typeof(Program));
 				foreach (var input in inputs)
 				{
 					Console.WriteLine();
-					Console.WriteLine(CliUtility.WordWrap(input.ReadToEnd(), 80));
+					Console.WriteLine(CliUtility.WordWrap(input.ReadToEnd(), wrap));
 
 				}
 			}
-			
-
 		}
 		/*
 				static void Main()
